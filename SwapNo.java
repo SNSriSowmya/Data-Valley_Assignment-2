0a169b5c1b522c02c0d51dc8f065a4b1d4572e0b
SNSriSowmya <132886390+SNSriSowmya@users.noreply.github.com>
2024-03-13 16:14:43 +0530

    SwapNo.java

--- SwapNo.java
+++ SwapNo.java
@@ -10,15 +10,15 @@ public class SwapNo
 		int b=300;
 		
 		System.out.println("Before swapping:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        	System.out.println("a = " + a);
+        	System.out.println("b = " + b);
         
-        a = a * b; 
-        b = a / b;  
-        a = a / b;  
+	        a = a * b; 
+	        b = a / b;  
+	        a = a / b;  
         
-        System.out.println("\nAfter swapping:");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+	        System.out.println("\nAfter swapping:");
+	        System.out.println("a = " + a);
+	        System.out.println("b = " + b);
 	}
 }
